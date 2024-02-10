a4a002142f5f3db61a0937c0b636d4c6f0b8490d
Abhishekcodex <104084107+Abhishekcodex@users.noreply.github.com>
2024-02-10 17:49:39 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    db: {
-      host: "localhost",
-      user: "root",
-      password: "G(t)d0nk2KTDe5gC",
-      database: "emedical",
-    },    
-  };
-  module.exports = config;
